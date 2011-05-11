1792a3d3c570b74ab23760582a446bbdea8b6c4c
Jace Bennett <jace.bennett@gmail.com>
2011-05-11 18:48:06 -0400

    Built GameBoard.render() interactively. May add test coverage later. For now, I'm not even confident in the data structure.

--- gameboard.py
+++ gameboard.py
@@ -1,6 +1,9 @@
 MAX_COLUMN_SIZE=6
 NUM_COLUMNS=7
 
+PLAYER_PIECE = '0'
+COMPUTER_PIECE = '1'
+
 class GameBoard(object):
 	def __init__(self):
 		self.columns = [];
@@ -8,5 +11,19 @@ class GameBoard(object):
 			self.columns.append([])
 
 
-	def add_piece(self, column_index):
-		self.columns[column_index].append(1)
+	def add_piece(self, pieceType, column_index):
+		self.columns[column_index].append(pieceType)
+
+	
+	def render(self):
+		for h in range(5, -1, -1):
+			row_str = ''
+			for w in range(NUM_COLUMNS):
+				if len(self.columns[w]) > h:
+					row_str += self.columns[w][h]
+				else:
+					row_str += "."
+			print row_str
+
+
+
