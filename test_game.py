af77c64ebede188d20d6e27a58139426aac8b286
Jace Bennett <jace.bennett@gmail.com>
2011-05-14 17:09:09 -0400

    More tests.

--- test_game.py
+++ test_game.py
@@ -4,87 +4,100 @@ from game import Game
 class PlayerStub(object):
 	"Stub"
 
-class GameTests(unittest.TestCase):
-	def test_player_toggling(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		self.assertNotEqual(player1, player2)
-		
-		game = Game([player1, player2])
-		self.assertEqual(game.current_player(), player1)
 
-		game.toggle_player()
-		self.assertEqual(game.current_player(), player2)
+player1, player2 = PlayerStub(), PlayerStub()
+
+class GameTests(unittest.TestCase):
+	def setUp(self):
+		self.game = Game([player1, player2])
+
+	def tearDown(self):
+		self.game = None
+
+	def test_player_toggling(self):
+		self.assertNotEqual(player1, player2)
+		self.assertEqual(self.game.current_player(), player1)
+
+		self.game.toggle_player()
+		
+		self.assertEqual(self.game.current_player(), player2)
+
+		self.game.toggle_player()
+		
+		self.assertEqual(self.game.current_player(), player1)
 
 	def test_recording_a_move_should_apply_the_move_to_the_board(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-
-		game.record_move(3)
-		self.assertEqual(game.board.get_piece_at(3,0), 0)
+		self.game.record_move(3)
+		self.assertEqual(self.game.board.get_piece_at(3,0), 0)
 	
 	def test_recording_a_move_should_log_it_in_history(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		
-		self.assertEqual(len(game.history), 0)
+		self.assertEqual(len(self.game.history), 0)
 
-		game.record_move(3)
+		self.game.record_move(3)
 		
-		self.assertEqual(len(game.history), 1)
-		self.assertEqual(game.last_move(), (3,0))
+		self.assertEqual(len(self.game.history), 1)
+		self.assertEqual(self.game.last_move(), (3,0))
 
-		game.record_move(4)
+		self.game.record_move(4)
 	
-		self.assertEqual(len(game.history), 2)
-		self.assertEqual(game.last_move(), (4,0))
+		self.assertEqual(len(self.game.history), 2)
+		self.assertEqual(self.game.last_move(), (4,0))
 
 	def test_recording_a_move_should_toggle_the_player(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		
-		self.assertEqual(game.current_player(), player1)
+		self.assertEqual(self.game.current_player(), player1)
 
-		game.record_move(3)
+		self.game.record_move(3)
 
-		self.assertEqual(game.current_player(), player2)
+		self.assertEqual(self.game.current_player(), player2)
 	
 	def test_rolling_back_moves_should_remove_them_from_the_board(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		game.record_move(3)
+		self.game.record_move(3)
 		
-		game.rollback(1)
+		self.game.rollback(1)
 
-		self.assertEqual(game.board.get_piece_at(3,0), None)
+		self.assertEqual(self.game.board.get_piece_at(3,0), None)
 	
 	def test_rolling_back_a_move_should_remove_it_from_history(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		game.record_move(3)
+		self.game.record_move(3)
 		
-		game.rollback(1)
+		self.game.rollback(1)
 		
-		self.assertEqual(len(game.history), 0)
+		self.assertEqual(len(self.game.history), 0)
 
 	def test_rolling_back_a_move_should_toggle_the_player(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		game.record_move(3)
+		self.game.record_move(3)
 		
-		self.assertEqual(game.current_player(), player2)
+		self.assertEqual(self.game.current_player(), player2)
 
-		game.rollback(1)
+		self.game.rollback(1)
 		
-		self.assertEqual(game.current_player(), player1)
+		self.assertEqual(self.game.current_player(), player1)
 
 	def test_undo_should_rollback_2_plays(self):
-		player1, player2 = PlayerStub(), PlayerStub()
-		game = Game([player1, player2])
-		
-		game.record_move(3)
-		game.record_move(4)
+		self.game.record_move(3)
+		self.game.record_move(4)
 
-		game.undo()
+		self.game.undo()
 
-		self.assertEqual(game.board.get_piece_at(3,0), None)
-		self.assertEqual(len(game.history), 0)
+		self.assertEqual(self.game.board.get_piece_at(3,0), None)
+		self.assertEqual(len(self.game.history), 0)
+
+	def test_undo_should_fail_on_current_players_first_move(self):
+		self.assertRaises(IndexError, lambda: self.game.undo())
+
+	def test_should_detect_victory(self):
+		self.game.record_move(3) #Player 1
+		self.game.record_move(6) #Player 2
+		self.game.record_move(2) #Player 1
+		self.game.record_move(6) #Player 2
+		self.game.record_move(1) #Player 1
+		self.game.record_move(6) #Player 2
+
+		self.assertFalse(self.game.is_won())
+
+		self.game.record_move(0) #Player 1
+
+		self.assertTrue(self.game.is_won())
+		self.assertEqual(self.game.winner, player1)
+
+
